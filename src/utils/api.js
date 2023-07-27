d1f65f13b277594fc10abdb7a77bf38c308ece4c
Alen <alensabu12xtz@gmail.com>
2023-07-28 01:05:35 +0530

    solving errors

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const BASE_URL = "http://api.themoviedb.org/3";
+const BASE_URL = "https://api.themoviedb.org/3";
 const TMDB_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MjMzNzA5Y2E1NzY2NjU5YTZkMWE5NDZlMWQ2NWExOSIsInN1YiI6IjY0YjY1NTI4YWM0MTYxMDBlMjQwNzg4YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.b8rQMQvGOVvm-4N2QQQZZBpLRn1yRP4P9IF4slLayfI";
 
 const headers = {
